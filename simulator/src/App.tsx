1acf60c5cd67ae8f6816022cc397f357a0b6a551
Jing Qiang <tohjingqiang@gmail.com>
2024-01-29 14:54:54 +0800

    simulator: add feature selector component

--- simulator/src/App.tsx
+++ simulator/src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationGrid } from "./components/core";
 import { Layout } from "./components/misc";
+import { AppFeatureSelector, AppFeaturesEnum } from "./components/common";
 
 function App() {
+  const [selectedFeature, setSelectedFeature] = useState<AppFeaturesEnum>(
+    AppFeaturesEnum.Algorithm
+  );
+
   return (
     <div id="app-container" className="font-poppins">
       <Layout>
+        <AppFeatureSelector
+          selectedFeature={selectedFeature}
+          setSelectedFeature={setSelectedFeature}
+        />
         <NavigationGrid />
       </Layout>
     </div>
---
+++ simulator/src/components/common/index.ts
@@ -0,0 +1,1 @@
+export { AppFeatureSelector, AppFeaturesEnum } from "./AppFeatureSelector";
