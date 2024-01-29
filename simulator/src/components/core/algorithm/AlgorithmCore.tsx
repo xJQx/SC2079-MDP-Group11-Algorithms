9fe02d5d827c198402efcecfa3b90b8cc8242c16
Jing Qiang <tohjingqiang@gmail.com>
2024-01-29 16:49:45 +0800

    simulator: implement basic NabigationGrid component

--- simulator/src/components/core/algorithm/AlgorithmCore.tsx
+++ simulator/src/components/core/algorithm/AlgorithmCore.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { NavigationGrid } from "./NavigationGrid";
 import { CoreContainter } from "../CoreContainter";
+import { Position } from "../../../schemas/robot";
+import { ROBOT_INITIAL_DIRECTION } from "../../../constants";
 
 export const AlgorithmCore = () => {
+  const robotPosition: Position = {
+    x: 0,
+    y: 0,
+    theta: ROBOT_INITIAL_DIRECTION,
+  };
+
   return (
     <CoreContainter title="Algorithm Simulator">
-      <NavigationGrid />
+      <NavigationGrid robotPosition={robotPosition} />
     </CoreContainter>
   );
 };
--- simulator/src/components/core/algorithm/NavigationGrid.tsx
+++ simulator/src/components/core/algorithm/NavigationGrid.tsx
@@ -1,5 +1,113 @@
 import React from "react";
+import { GRID_TOTAL_HEIGHT, GRID_TOTAL_WIDTH } from "../../../constants";
+import { Position } from "../../../schemas/robot";
 
-export const NavigationGrid = () => {
-  return <div>NavigationGrid</div>;
+interface NavigationGridProps {
+  robotPosition: Position;
+}
+
+export const NavigationGrid = (props: NavigationGridProps) => {
+  const { robotPosition } = props;
+
+  /**
+   * Creates a HTML Grid.
+   * @abstract This function will look at the state of the cell and style it accordingly. E.g.: Empty cell vs Cell with Robot.
+   * @returns React.ReactNode[][] - `<td />[][]`
+   * */
+  const createHTMLGrid = () => {
+    const grid: React.ReactNode[][] = [];
+
+    for (let y = GRID_TOTAL_HEIGHT - 1; y >= 0; y--) {
+      const currentRow: React.ReactNode[] = [];
+
+      for (let x = 0; x < GRID_TOTAL_WIDTH; x++) {
+        // Cell Contains Robot.
+        if (
+          robotPosition.x <= x &&
+          x <= robotPosition.x + 2 &&
+          robotPosition.y <= y &&
+          y <= robotPosition.y + 2
+        )
+          currentRow.push(
+            createHTMLGridCellRobot(
+              x,
+              y,
+              x === robotPosition.x + 1 && y === robotPosition.y + 2
+                ? "camera"
+                : "body"
+            )
+          );
+        // Empty Cell
+        else currentRow.push(createHTMLGridCellEmpty(x, y));
+      }
+      grid.push(currentRow);
+    }
+    return grid;
+  };
+
+  const grid = createHTMLGrid();
+  addHTMLGridLables(grid);
+
+  return (
+    <div>
+      {/* Grid */}
+      <table>
+        <tbody>
+          {grid.map((row) => {
+            return <tr>{row.map((column) => column)}</tr>;
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+// ---------- Helper Functions ---------- //
+/**
+ * Creates a `<td />` for an empty cell
+ * @used_by createHTMLGrid()
+ */
+const createHTMLGridCellEmpty = (x: number, y: number) => {
+  return (
+    <td id={`cell-${x}-${y}`} className="border border-orange-900 w-8 h-8" />
+  );
+};
+
+/**
+ * Creates a `<td />` for a cell that contains the Robot's body
+ * @used_by createHTMLGrid()
+ */
+const createHTMLGridCellRobot = (
+  x: number,
+  y: number,
+  type: "camera" | "body"
+) => {
+  return (
+    <td
+      id={`cell-${x}-${y}`}
+      className={`border border-orange-900 w-8 h-8 ${
+        type === "body" ? "bg-green-500" : "bg-yellow-500"
+      }`}
+    />
+  );
+};
+
+/** Adds x-axis and y-axis labels to the Grid */
+const addHTMLGridLables = (grid: React.ReactNode[][]) => {
+  grid.forEach((row, index) =>
+    row.unshift(
+      <td className="font-bold pr-2">{GRID_TOTAL_HEIGHT - index - 1}</td>
+    )
+  );
+
+  const gridColumnLabels: React.ReactNode[] = [];
+  for (let c = -1; c < GRID_TOTAL_WIDTH; c++) {
+    if (c === -1) gridColumnLabels.push(<td />);
+    else
+      gridColumnLabels.push(
+        <td className="font-bold pt-2 text-center">{c}</td>
+      );
+  }
+  grid.push(gridColumnLabels);
+  return grid;
 };
