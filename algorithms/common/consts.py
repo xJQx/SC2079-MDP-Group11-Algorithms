442adb0c0ca1da32bfa9dce02fbfb241d77a6f64
Jing Qiang <tohjingqiang@gmail.com>
2024-03-15 19:48:06 +0800

    algorithms: update A/B indoor + add an multiplier for priority obstacles backwards X and Y bound to increase accuracy of collision detection

--- algorithms/common/consts.py
+++ algorithms/common/consts.py
@@ -16,8 +16,11 @@ ROBOT_VERT_OFFSET = (ROBOT_HEIGHT - ROBOT_ACTUAL_HEIGHT) / 2
 # +--------------------+
 # | collision checking |
 # +--------------------+
-# WPS: Waypoints
-# Measure (Using Bottom Left of Robot)
+"""
+  # TODO: Measureable
+  WPS: Waypoints
+  Measure using Bottom Left of Robot
+"""
 # Indoor (SCSE Lounge)
 # WPS_FL_IN = [(0, 0, 0), (1.1375, 6.125, 0), (-1.1375, 11.375, 0), (-2.8438, 15.75, 0), (-6.825, 19.25, 0), (-15.3563, 24.5, 0), (-22.75, 26.25, 1.5707963267948966)]
 # WPS_FR_IN = [(0, 0, 0), (1.4217, 11.9025, 0), (6.1389, 25.3575, 0), (8.9722, 30.5325, 0), (11.8056, 34.6725, 0), (15.1111, 38.8125, 0), (19.3611, 41.9175, 0), (23.6111, 45.0225, 0), (27.8611, 48.1275, 0), (42.5, 51.75, -1.5707963267948966)]
@@ -26,17 +29,17 @@ ROBOT_VERT_OFFSET = (ROBOT_HEIGHT - ROBOT_ACTUAL_HEIGHT) / 2
 
 # Measure (Using Bottom Left of Robot)
 # Indoor (SCSE Lab) - Radians are estimation (Start-End)/number_of_points
-WPS_FL_IN = [(0, 0, 0), (1.1375, 6.125, 0.2618), (-1.1375, 11.375, 0.5236), (-2.8438, 15.75, 0.7854), (-6.825, 19.25, 1.0472), (-15.3563, 24.5, 1.3090), (-22.75, 26.25, 1.5707963267948966)]
-WPS_FR_IN = [(0, 0, 0), (1.4217, 11.9025, -0.1745), (6.1389, 25.3575, -0.3491), (8.9722, 30.5325, -0.5236), (11.8056, 34.6725, -0.6981), (15.1111, 38.8125, -0.8727), (19.3611, 41.9175, -1.0472), (23.6111, 45.0225, -1.2217), (27.8611, 48.1275, -1.3963), (43, 52, -1.5707963267948966)]
-WPS_BR_IN = [(0, 0, 0), (0.7125, -5.8875, 0.2618), (2.4938, -11.775, 0.5236), (6.4125, -19.625, 0.7854), (14.9625, -29.4375, 1.0472), (27.7875, -36.6333, 1.3090), (42.75, -39.25, 1.5707963267948966)]
-WPS_BL_IN = [(0, 0, 0), (-0.9613, -5.475, -0.2618), (-3.2042, -11.315, -0.5236), (-6.7289, -15.695, -0.7854), (-12.1761, -18.25, -1.0472), (-16.3415, -20.075, -1.3090), (-22.75, -18.25, -1.5707963267948966)]
+WPS_FL_IN = [(1.1375, 6.125, 0.2618), (-1.1375, 11.375, 0.5236), (-2.8438, 15.75, 0.7854), (-6.825, 19.25, 1.0472), (-15.3563, 24.5, 1.3090), (-18.5, 26.5, 1.5707963267948966)]
+WPS_FR_IN = [(1.4217, 11.9025, -0.1745), (6.1389, 25.3575, -0.3491), (8.9722, 30.5325, -0.5236), (11.8056, 34.6725, -0.6981), (15.1111, 38.8125, -0.8727), (19.3611, 41.9175, -1.0472), (23.6111, 45.0225, -1.2217), (27.8611, 48.1275, -1.3963), (43, 52, -1.5707963267948966)]
+WPS_BR_IN = [(0.7125, -5.8875, 0.2618), (2.4938, -11.775, 0.5236), (6.4125, -19.625, 0.7854), (14.9625, -29.4375, 1.0472), (27.7875, -36.6333, 1.3090), (44.5, -39.5, 1.5707963267948966)]
+WPS_BL_IN = [(-0.9613, -5.475, -0.2618), (-3.2042, -11.315, -0.5236), (-6.7289, -15.695, -0.7854), (-12.1761, -18.25, -1.0472), (-16.3415, -20.075, -1.3090), (-20, -16, -1.5707963267948966)]
 
 # TODO: Measure (Using Bottom Left of Robot)
 # Outdoor (SCSE Corridor)
-WPS_FL_OUT = [(0, 0, 0), (1.1375, 6.125, 0), (-1.1375, 11.375, 0), (-2.8438, 15.75, 0), (-6.825, 19.25, 0), (-15.3563, 24.5, 0), (-22.75, 26.25, 1.5707963267948966)]
-WPS_FR_OUT = [(0, 0, 0), (1.4217, 11.9025, 0), (6.1389, 25.3575, 0), (8.9722, 30.5325, 0), (11.8056, 34.6725, 0), (15.1111, 38.8125, 0), (19.3611, 41.9175, 0), (23.6111, 45.0225, 0), (27.8611, 48.1275, 0), (43, 52, -1.5707963267948966)]
-WPS_BR_OUT = [(0, 0, 0), (0.7125, -5.8875, 0), (2.4938, -11.775, 0), (6.4125, -19.625, 0), (14.9625, -29.4375, 0), (27.7875, -36.6333, 0), (42.75, -39.25, 1.5707963267948966)]
-WPS_BL_OUT = [(0, 0, 0), (-0.9613, -5.475, 0), (-3.2042, -11.315, 0), (-6.7289, -15.695, 0), (-12.1761, -18.25, 0), (-16.3415, -20.075, 0), (-22.75, -18.25, -1.5707963267948966)]
+WPS_FL_OUT = [(1.1375, 6.125, 0.2618), (-1.1375, 11.375, 0.5236), (-2.8438, 15.75, 0.7854), (-6.825, 19.25, 1.0472), (-15.3563, 24.5, 1.3090), (-18.5, 26.5, 1.5707963267948966)]
+WPS_FR_OUT = [(1.4217, 11.9025, -0.1745), (6.1389, 25.3575, -0.3491), (8.9722, 30.5325, -0.5236), (11.8056, 34.6725, -0.6981), (15.1111, 38.8125, -0.8727), (19.3611, 41.9175, -1.0472), (23.6111, 45.0225, -1.2217), (27.8611, 48.1275, -1.3963), (43, 52, -1.5707963267948966)]
+WPS_BR_OUT = [(0.7125, -5.8875, 0.2618), (2.4938, -11.775, 0.5236), (6.4125, -19.625, 0.7854), (14.9625, -29.4375, 1.0472), (27.7875, -36.6333, 1.3090), (44.5, -39.5, 1.5707963267948966)]
+WPS_BL_OUT = [(-0.9613, -5.475, -0.2618), (-3.2042, -11.315, -0.5236), (-6.7289, -15.695, -0.7854), (-12.1761, -18.25, -1.0472), (-16.3415, -20.075, -1.3090), (-20, -16, -1.5707963267948966)]
 
 WPS_FL = WPS_FL_IN if INDOOR else WPS_FL_OUT
 WPS_FR = WPS_FR_IN if INDOOR else WPS_FR_OUT
@@ -44,22 +47,25 @@ WPS_BR = WPS_BR_IN if INDOOR else WPS_BR_OUT
 WPS_BL = WPS_BL_IN if INDOOR else WPS_BL_OUT
 
 BUFFER = 5.01
-OUTER_ARC_POINTS = 20
-INNER_ARC_POINTS = 10
 
 # +-------+
 # | astar |
 # +-------+
+"""
+  # TODO: Measureable -> Take value from Final Displacement
+  Ellipse Equation's `A` and `B` where `A` is the X radius / semi-major axis, and `B` is the Y radius / semi-minor axis
+"""
+# Indoor (SCSE Lab)
+FL_A_IN, FL_B_IN = 18.5, 26.5
+FR_A_IN, FR_B_IN = 43, 52
+BR_A_IN, BR_B_IN = 44.5, 39.5
+BL_A_IN, BL_B_IN = 20, 16
 
-FL_A_IN, FL_B_IN = 15.6, 21.6 # TODO: Calculate
-FR_A_IN, FR_B_IN = 35.2, 40.6 # TODO: Calculate
-BR_A_IN, BR_B_IN = 42.7, 34.1 # TODO: Calculate
-BL_A_IN, BL_B_IN = 21.3, 14.9 # TODO: Calculate
-
-FL_A_OUT, FL_B_OUT = 15.1, 22.2 # TODO: Calculate
-FR_A_OUT, FR_B_OUT = 33.1, 40.7 # TODO: Calculate
-BR_A_OUT, BR_B_OUT = 41.6, 34 # TODO: Calculate
-BL_A_OUT, BL_B_OUT = 20.8, 14.2 # TODO: Calculate
+# Outdoor (SCSE Corridor)
+FL_A_OUT, FL_B_OUT = 15.1, 22.2 # TODO: Measure
+FR_A_OUT, FR_B_OUT = 33.1, 40.7 # TODO: Measure
+BR_A_OUT, BR_B_OUT = 41.6, 34 # TODO: Measure
+BL_A_OUT, BL_B_OUT = 20.8, 14.2 # TODO: Measure
 
 FL_A, FL_B = (FL_A_IN, FL_B_IN) if INDOOR else (FL_A_OUT, FL_B_OUT)
 FR_A, FR_B = (FR_A_IN, FR_B_IN) if INDOOR else (FR_A_OUT, FR_B_OUT)
@@ -94,6 +100,7 @@ CONE = [10, 10, 4, 40]
 # +--------------------+
 # | Priority obstacles |
 # +--------------------+
+"""For identifying obstacles that are potentially in the path of the robot"""
 
 # *_X = LEFT, RIGHT
 # *_Y = UP, DOWN
@@ -113,6 +120,8 @@ FR_OUTER = FR_OUTER_IN if INDOOR else FR_OUTER_OUT
 BL_OUTER = BL_OUTER_IN if INDOOR else BL_OUTER_OUT
 BR_OUTER = BR_OUTER_IN if INDOOR else BR_OUTER_OUT
 
+# [TODO: Editable] To increase the boundaries to identify obstacles potentially in the path of the robot more accurately (before doing obstacle collision detection)
+BACKWARDS_A_B_MULTIPLIER = 1.5
 
 FL_X_BOUND = [OBSTACLE_WIDTH/2 + FL_A - ROBOT_WIDTH/2 + ROBOT_HEIGHT - ROBOT_VERT_OFFSET, 
               OBSTACLE_WIDTH/2 + ROBOT_WIDTH]
@@ -126,17 +135,17 @@ FR_X_BOUND = [OBSTACLE_WIDTH/2,
 FR_Y_BOUND = [OBSTACLE_WIDTH/2 + FR_OUTER + (FR_B - FL_A) + ROBOT_VERT_OFFSET, 
               OBSTACLE_WIDTH/2]
 
-BL_X_BOUND = [OBSTACLE_WIDTH/2 + BL_A - ROBOT_WIDTH/2 + ROBOT_VERT_OFFSET, 
-              OBSTACLE_WIDTH/2 + BL_OUTER - (BL_A - ROBOT_WIDTH/2)] 
+BL_X_BOUND = [OBSTACLE_WIDTH/2 + (BL_A*BACKWARDS_A_B_MULTIPLIER) - ROBOT_WIDTH/2 + ROBOT_VERT_OFFSET, 
+              OBSTACLE_WIDTH/2 + BL_OUTER - ((BL_A*BACKWARDS_A_B_MULTIPLIER) - ROBOT_WIDTH/2)] 
 
 BL_Y_BOUND = [OBSTACLE_WIDTH/2 + ROBOT_HEIGHT, 
-              OBSTACLE_WIDTH/2 + BL_B + ROBOT_WIDTH/2 - ROBOT_VERT_OFFSET]
+              OBSTACLE_WIDTH/2 + (BL_B*BACKWARDS_A_B_MULTIPLIER) + ROBOT_WIDTH/2 - ROBOT_VERT_OFFSET]
 
-BR_X_BOUND = [OBSTACLE_WIDTH/2 + BR_OUTER - BR_A - ROBOT_WIDTH/2, 
-              OBSTACLE_WIDTH/2 + BR_A + ROBOT_WIDTH/2 + ROBOT_VERT_OFFSET]
+BR_X_BOUND = [OBSTACLE_WIDTH/2 + BR_OUTER - (BR_A*BACKWARDS_A_B_MULTIPLIER) - ROBOT_WIDTH/2, 
+              OBSTACLE_WIDTH/2 + (BR_A*BACKWARDS_A_B_MULTIPLIER) + ROBOT_WIDTH/2 + ROBOT_VERT_OFFSET]
 
 BR_Y_BOUND = [OBSTACLE_WIDTH/2 + ROBOT_HEIGHT, 
-              OBSTACLE_WIDTH/2 + BR_B + ROBOT_WIDTH/2 - ROBOT_VERT_OFFSET]
+              OBSTACLE_WIDTH/2 + (BR_B*BACKWARDS_A_B_MULTIPLIER) + ROBOT_WIDTH/2 - ROBOT_VERT_OFFSET]
 
 ROBOT_BTM_LEFT_CIRCLE_RAD = 2 # indicates bottom left of the robot footprint (used in algorithms/simulator)
 ROBOT_MIN_CAMERA_DIST = 20
