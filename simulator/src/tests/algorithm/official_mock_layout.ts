880f951cda56f9ca93c36eda83380038faa81280
Jing Qiang <tohjingqiang@gmail.com>
2024-03-22 08:56:50 +0800

    simulator: update mock layout test

--- simulator/src/tests/algorithm/official_mock_layout.ts
+++ simulator/src/tests/algorithm/official_mock_layout.ts
@@ -4,7 +4,7 @@ import { ObstacleDirection } from "../../schemas/obstacle";
 export const AlgoTestOfficialMockLayout: AlgoTestDataInterface = {
   obstacles: [
     { id: 1, x: 5, y: 9, d: ObstacleDirection.S },
-    { id: 2, x: 8, y: 14, d: ObstacleDirection.W },
+    { id: 2, x: 7, y: 14, d: ObstacleDirection.W },
     { id: 3, x: 12, y: 9, d: ObstacleDirection.E },
     { id: 4, x: 15, y: 4, d: ObstacleDirection.W },
     { id: 5, x: 15, y: 15, d: ObstacleDirection.S },
